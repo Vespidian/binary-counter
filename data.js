54aa6c7cfa2f1518e1f851cfea20b25058ad98af
Pimy <Vespimian@gmail.com>
2018-04-14 22:06:07 -0400

    Initalize

---
+++ data.js
@@ -0,0 +1,31 @@
+var data = [
+
+	{
+		
+	},
+	{
+		
+	},
+	
+];
+
+var bit0 = 0;
+var bit1 = 0;
+var bit2 = 0;
+var bit3 = 0;
+var bit4 = 0;
+var bit5 = 0;
+var bit6 = 0;
+var bit7 = 0;
+
+var bitEq0 = 1;
+var bitEq1 = 2;
+var bitEq2 = 4;
+var bitEq3 = 8;
+var bitEq4 = 16;
+var bitEq5 = 32;
+var bitEq6 = 64;
+var bitEq7 = 128;
+
+var result = 0;
+var binaryResult = "";
